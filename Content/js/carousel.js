b796da99553bb97d9a525689b478120f0758b9fa
Anoop Peter <anoop.peterkv@gmail.com>
2022-08-18 22:33:25 +0530

    new testimonial

--- Content/js/carousel.js
+++ Content/js/carousel.js
@@ -1,34 +1,59 @@
-jQuery(function ($) {
-    "use strict";
-    var $window = $(window);
-    var windowsize = $(window).width();
-    var $root = $("html, body");
-    var $this = $(this);
- 
- 
-    /*Testimonials 3columns*/
-    $("#testimonial-slider").owlCarousel({
-       items: 3,
-       autoplay: 2500,
-       autoplayHoverPause: true,
-       loop: true,
-       margin: 30,
-       dots: true,
-       nav: false,
-       responsive: {
-          1280: {
-             items: 3,
-          },
-          600: {
-             items: 2,
-          },
-          320: {
-             items: 1,
-          },
-       }
-    });
- 
- 
- });
- 
- 
+// coding with nick
+// vars
+'use strict'
+var testim = document.getElementById("testim"),
+    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimleftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer
+    ;
+
+window.onload = function () {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length - 1;
+        }
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function () {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+// coding with nick
+    testimleftArrow.addEventListener("click", function () {
+        playSlide(currentSlide -= 1);
+    })
+    testimRightArrow.addEventListener("click", function () {
+        playSlide(currentSlide += 1);
+    })
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function () {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+    playSlide(currentSlide);
+
+}
+
